5ddd5ca5d2bfcf8ee51d9251ee933d5819edc97e
Ogunlewe Al Ameen <ogunlewealameen08@gmail.com>
2024-11-14 17:00:15 +0000

    feat

--- src/js/apply.js
+++ src/js/apply.js
@@ -1,35 +1,58 @@
-const sections = document.querySelectorAll('.fade-element');
-const progressIndicator = document.getElementById('progress-indicator');
+const sections = document.querySelectorAll(".fade-element");
+const icons = document.querySelectorAll(".icon");
+const progressIndicator = document.getElementById("progress-indicator");
+const progressStep = document.getElementById("progress-step");
+const  button = document.getElementById("button");
+
+
+
+
 let currentSectionIndex = 0;
 let isTransitioning = false;
 
-
 function nextQuestion() {
-    if (isTransitioning) return;
-    isTransitioning = true;
+  if (isTransitioning) return;
+  isTransitioning = true;
 
-    const currentSection = sections[currentSectionIndex];
-    currentSection.classList.add('hidden');
+  const currentSection = sections[currentSectionIndex];
+  const currentIcon = icons[currentSectionIndex];
 
-   
-    currentSection.addEventListener('transitionend', function handler() {
-        currentSection.removeEventListener('transitionend', handler);
+  currentSection.classList.add("hidden");
+  currentIcon.classList.remove("show");
 
-      
-        if (currentSectionIndex < sections.length - 1) {
-            currentSectionIndex++;
-            sections[currentSectionIndex].classList.remove('hidden');
+  currentSection.addEventListener("transitionend", function handler() {
+    currentSection.removeEventListener("transitionend", handler);
 
-          
-            updateProgressIndicator();
-        }
+    if (currentSectionIndex < sections.length - 1) {
+      currentSectionIndex++;
+      const nextSection = sections[currentSectionIndex];
+      const nextIcon = icons[currentSectionIndex];
 
-        isTransitioning = false;
-    });
+      nextSection.classList.remove("hidden");
+      nextIcon.classList.add("show");
+
+      updateProgressIndicator();
+    }
+
+    isTransitioning = false;
+  });
 }
-
 
 function updateProgressIndicator() {
-    const progressPercentage = ((currentSectionIndex + 1) / sections.length) * 100;
-    progressIndicator.style.width = progressPercentage + '%';
+  const progressPercentage =
+    ((currentSectionIndex + 1) / sections.length) * 100;
+  progressIndicator.style.width = `${progressPercentage}%`;
+
+  const containerWidth = document.querySelector(
+    ".progress-container"
+  ).offsetWidth;
+  const stepPosition = (progressPercentage / 100) * containerWidth;
+  progressStep.style.transform = `translateX(${stepPosition - 15}px)`;
 }
+
+buttons.forEach(element => {
+  button.onclick = function() {
+    button.style.background ="green";
+  }  
+});
+
